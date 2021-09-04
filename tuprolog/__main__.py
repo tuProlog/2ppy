15f7b23b93f5bb696542f6f2a3bafa76fa1e594b
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2021-09-04 12:20:42 +0200

    update tuprolog.__main__

--- tuprolog/__main__.py
+++ tuprolog/__main__.py
@@ -5,9 +5,11 @@ from tuprolog.core.exception import *
 from tuprolog.core.formatters import *
 from tuprolog.core.operators import *
 from tuprolog.core.visitors import *
+from tuprolog.core.parsing import *
 from tuprolog.unify import *
-from tuprolog.solve import *
+from tuprolog.theory import *
 from tuprolog.theory.parsing import *
+from tuprolog.solve import *
 
 import code
 
