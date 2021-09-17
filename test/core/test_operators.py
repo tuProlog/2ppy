7d4db73b1856ab087d2300507995efbb59f65a2d
AndAgio <aagiollo@gmail.com>
2021-09-17 18:46:26 +0200

    WIP - test operators started

--- test/core/test_operators.py
+++ test/core/test_operators.py
@@ -1,4 +1,4 @@
-from tuprolog.core.operators import EMPTY_OPERATORS
+from tuprolog.core.operators import EMPTY_OPERATORS, XF, XFY, YF, YFX, FX, FY, XFX, operator, specifier, Specifier
 import unittest
 from tuprolog import *
 from tuprolog.core import *
@@ -6,10 +6,27 @@ from tuprolog.core import *
 class TestOperators(unittest.TestCase):
 
     def setUp(self):
-        self._empty_operator = EMPTY_OPERATORS
+        self.constants = {XF, YF, FX, FY, XFY, YFX, XFX}
+        self.my_operator = [operator('a', const, 1) for const in self.constants]
+        self.operator1 = operator('a', XF, 1)
+        self.operator2 = operator('a', XF, 1)
+        self.my_operator_logic = [operator(integer(1), const.toTerm(), atom('a')) for const in self.constants]
+        self.my_operator_struct = [operator(struct('op', integer(1), const.toTerm(), atom('a'))) for const in self.constants]
 
-    def test_operator(self):
-        self.assertEqual(self._empty_operator, None)
+    def test_operator_construction(self):
+        for operators in [self.my_operator, self.my_operator_logic, self.my_operator_struct]:
+            for operator in operators:
+                self.assertEqual(operator.getFunctor(), 'a')
+                self.assertTrue(operator.getSpecifier() in self.constants)
+                self.assertEqual(operator.getPriority(), 1)
+                self.assertEqual(operator.functor, 'a')
+                self.assertTrue(operator.specifier in self.constants)
+                self.assertEqual(operator.priority, 1)
+
+    def test_operator_equality(self):    
+        self.assertEqual(self.operator1, self.operator2)
+        self.assertTrue(self.operator1 == self.operator2)
+
  
 
 
