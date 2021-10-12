517084aa169bd9aac4043291e2b770011daee250
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2021-10-12 11:25:23 +0200

    [wip] auto property on methods

--- tuprolog/core/_ktadapt.py
+++ tuprolog/core/_ktadapt.py
@@ -2,72 +2,74 @@ from tuprolog import logger
 
 import jpype
 
+from tuprolog.jvmutils import pythonize_properties
+
 
 @jpype.JImplementationFor("it.unibo.tuprolog.core.Term")
 class _KtTerm:
     def __jclass_init__(self):
-        pass
+        pythonize_properties(self)
 
     def __getitem__(self, item, *items):
         return self.get(item, *items)
 
-    @property
-    def variables(self):
-        return self.getVariables()
+    # @property
+    # def variables(self):
+    #     return self.getVariables()
 
 
 @jpype.JImplementationFor("it.unibo.tuprolog.core.operators.Operator")
 class _KtOperator:
     def __jclass_init__(self):
-        pass
+        pythonize_properties(self)
 
-    @property
-    def functor(self):
-        return self.getFunctor()
-
-    @property
-    def specifier(self):
-        return self.getSpecifier()
-
-    @property
-    def priority(self):
-        return self.getPriority()
+    # @property
+    # def functor(self):
+    #     return self.getFunctor()
+    #
+    # @property
+    # def specifier(self):
+    #     return self.getSpecifier()
+    #
+    # @property
+    # def priority(self):
+    #     return self.getPriority()
 
 
 @jpype.JImplementationFor("it.unibo.tuprolog.core.Struct")
 class _KtStruct:
     def __jclass_init__(self):
-        pass
+        pythonize_properties(self)
 
-    @property
-    def functor(self):
-        return self.getFunctor()
-
-    @property
-    def args(self):
-        return self.getArgs()
-
-    @property
-    def arity(self):
-        return self.getArity()
+    # @property
+    # def functor(self):
+    #     return self.getFunctor()
+    #
+    # @property
+    # def args(self):
+    #     return self.getArgs()
+    #
+    # @property
+    # def arity(self):
+    #     return self.getArity()
 
 
 @jpype.JImplementationFor("it.unibo.tuprolog.core.Clause")
 class _KtClause:
     def __jclass_init__(self):
-        pass
+        pythonize_properties(self)
 
-    @property
-    def head(self):
-        return self.getHead()
-
-    @property
-    def body(self):
-        return self.getBody()
-
-    @property
-    def is_well_formed(self):
-        return self.isWellFormed()
+    # @property
+    # def head(self):
+    #     return self.getHead()
+    #
+    # @property
+    # def body(self):
+    #     return self.getBody()
+    #
+    # @property
+    # def is_well_formed(self):
+    #     return self.isWellFormed()
 
 
 logger.debug("Configure Kotlin adapters for types in it.unibo.tuprolog.core.*")
--- tuprolog/jvmutils.py
+++ tuprolog/jvmutils.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 from tuprolog import logger
 # noinspection PyUnresolvedReferences
 import jpype
@@ -21,6 +23,7 @@ from typing import Iterator as PyIterator
 from typing import Mapping, MutableMapping, Callable, Any
 
 from .jvmioutils import *
+import re
 
 
 Arrays = _jutils.Arrays
@@ -48,6 +51,134 @@ Sequence = _ksequences.Sequence
 SequencesKt = _ksequences.SequencesKt
 
 PyUtils = _tuprolog_utils.PyUtils
+
+class JvmProperty:
+
+    @staticmethod
+    def pythonize_name(name: str, boolean: bool = False):
+        name = name[2:] if boolean else name[3:]
+        words = re.findall('([A-Z]+[a-z]*)|[0-9]+', name)
+        if len(words) == 0:
+            return name
+        snake_case = "_".join(map(str.lower, words))
+        if boolean:
+            return "is_" + snake_case
+        else:
+            return snake_case
+
+    def __init__(self, getter_name: str = None, getter = None, setter_name: str = None, setter = None):
+        assert (getter_name is not None and getter is not None) or (setter_name is not None and setter is not None)
+        self._getter_name = getter_name
+        self._getter = getter
+        self._setter_name = setter_name
+        self._setter = setter
+
+    @property
+    def is_readonly(self):
+        return self._setter is None
+
+    @property
+    def is_writeonly(self):
+        return self._getter is None
+
+    @property
+    def is_boolean(self):
+        return not self.is_writeonly and self._getter_name.startswith('is')
+
+    @property
+    @cache
+    def python_name(self):
+        if self.is_readonly:
+            return JvmProperty.pythonize_name(self._getter_name, self.is_boolean)
+        else:
+            return JvmProperty.pythonize_name(self._setter_name, self.is_boolean)
+
+    @property
+    def property(self):
+        if self.is_readonly:
+            return property(fget=self._getter)
+        elif self.is_writeonly:
+            return property(fset=self._setter)
+        else:
+            return property(fget=self._getter, fset=self._setter)
+
+    def set_getter(self, name, method):
+        return JvmProperty(name, method, self._setter_name, self._setter)
+
+    def set_setter(self, name, method):
+        return JvmProperty(self._getter_name, self._getter, name, method)
+
+    def merge(self, other):
+        if self.is_readonly:
+            return self.set_setter(other._setter_name, other._setter)
+        elif self.is_writeonly:
+            return self.set_getter(other._getter_name, other._getter)
+        else:
+            return self
+
+    def __eq__(self, other):
+        if self is other:
+            return True
+        if not isinstance(other, JvmProperty):
+            return False
+        return self._getter_name == other._getter_name \
+            and self._setter_name == other._setter_name \
+            and self._getter == other._getter \
+            and self._setter == other._setter
+
+    def __hash__(self):
+        return hash((self._getter_name, self._getter, self._setter, self._setter_name))
+
+    def __repr__(self):
+        return f"JvmProperty({self.python_name}, {self._getter_name or '<no getter>'}" + \
+               f", {self._setter_name or '<no setter>'})"
+
+    def __str__(self):
+        return repr(self)
+
+    @staticmethod
+    def from_class(cls):
+        def is_getter_or_setter(method):
+            return is_getter(method) or is_setter(method)
+
+        def is_getter(method):
+            return method != 'getClass' and method.startswith('get') or method.startswith('is')
+
+        def is_setter(method):
+            return method.startswith('set')
+
+        def setter_from_getter(method):
+            return 'set' + method[2 if method.startswith('is') else 3]
+
+        def is_zero_args_method(cls, method):
+            try:
+                cls.class_.getMethod(method, [])
+                return True
+            except:
+                return False
+
+        properties = dict()
+
+        for method in cls.__dict__:
+            if is_getter_or_setter(method) and is_zero_args_method(cls, method):
+                if is_getter(method):
+                    property = JvmProperty(getter_name=method, getter=cls.__dict__[method])
+                elif is_setter(method):
+                    property = JvmProperty(setter_name=method, setter=cls.__dict__[method])
+                else:
+                    continue
+                if property.python_name in properties:
+                    properties[property.python_name] = properties[property.python_name].merge(property)
+                else:
+                    properties[property.python_name] = property
+
+        return properties.values()
+
+
+def pythonize_properties(cls):
+    properties = JvmProperty.from_class(cls)
+    for p in properties:
+        cls[p.python_name] = p.property
 
 
 @jpype.JImplements("java.util.Iterator", deferred=True)
