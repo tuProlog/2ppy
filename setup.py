65932234544f87ec1cd5697b4f6156def7cb143d
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-04-04 20:18:16 +0200

    Fix setup debug info

--- setup.py
+++ setup.py
@@ -16,14 +16,14 @@ def download_jars():
     proc = subprocess.Popen(MAVEN_EXECUTABLE + ['-version'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     stdout, stderr = proc.communicate()
     if proc.returncode != 0:
-        raise RuntimeError(f'Could not run mvn.cmd: {stderr}')
+        raise RuntimeError(f'Could not run mvn:\n{stdout}\n{stderr}')
     if 'Apache Maven' not in stdout:
         raise RuntimeError(f'Could not find Apache Maven in {stdout}')
     print('Downloading JARs...')
     proc = subprocess.Popen(MAVEN_EXECUTABLE + ['dependency:copy-dependencies', f'-DoutputDirectory={JAR_FOLDER}'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, cwd=Path(__file__).parent)
     stdout, stderr = proc.communicate()
     if proc.returncode != 0:
-        raise RuntimeError(f'Error while downloading JARs: {stdout} {stderr}')
+        raise RuntimeError(f'Error while downloading JARs:\n{stdout}\n{stderr}')
 
 
 class BuildPyCommand(build_py):
