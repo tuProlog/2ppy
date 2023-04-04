bdb7507b3bc5eb1e57a95b6d33a27493d33f794c
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-04-04 12:32:54 +0200

    Fix missing jre for java 16

--- setup.py
+++ setup.py
@@ -64,9 +64,9 @@ def install_java():
         print('Java already installed')
         return
     java_version = os.getenv('JAVA_VERSION', '11')
-    destination_folder = str(JAR_FOLDER.resolve())
+    destination_folder = str(JAR_FOLDER)
     print(f'Downloading Java {java_version} in {destination_folder}...')
-    installation_path = Path(jdk.install(java_version, jre=True, path=destination_folder))
+    installation_path = Path(jdk.install(java_version, jre=not java_version.startswith('16'), path=destination_folder))
     destination_folder = JAVA_FOLDER
     print(f'Installing Java in {destination_folder}...')
     installation_path = installation_path.rename(destination_folder)
