d4c533fe09da9fee526034dbf056718161e9c399
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-04-05 16:21:50 +0200

    Refactor ci/cd

--- setup.py
+++ setup.py
@@ -17,14 +17,12 @@ def is_windows():
 
 
 def download_jars():
-    print('Checking Maven...')
     proc = subprocess.Popen(MAVEN_EXECUTABLE + ['-v'], shell=is_windows(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     stdout, stderr = proc.communicate()
     if proc.returncode != 0:
         raise RuntimeError(f'Could not run mvn:\n{stdout}\n{stderr}')
     if 'Apache Maven' not in stdout:
         raise RuntimeError(f'Could not find Apache Maven in {stdout}')
-    print('Downloading JARs...')
     proc = subprocess.Popen(MAVEN_EXECUTABLE + ['dependency:copy-dependencies', f'-DoutputDirectory={JAR_FOLDER}'], shell=is_windows(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, cwd=Path(__file__).parent)
     stdout, stderr = proc.communicate()
     if proc.returncode != 0:
@@ -39,16 +37,12 @@ class BuildPyCommand(build_py):
 
 def install_java():
     if JAVA_FOLDER.exists():
-        print('Java already installed')
         return
     java_version = os.getenv('JAVA_VERSION', '11')
     destination_folder = str(JAR_FOLDER)
-    print(f'Downloading Java {java_version} in {destination_folder}...')
     installation_path = Path(jdk.install(java_version, jre=not java_version.startswith('16'), path=destination_folder))
     destination_folder = JAVA_FOLDER
-    print(f'Installing Java in {destination_folder}...')
     installation_path = installation_path.rename(destination_folder)
-    print(f'Installed Java in {installation_path}')
 
 
 install_java()
