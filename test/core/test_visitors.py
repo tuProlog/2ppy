ee576501a6d0cc0007c86f9896c30e8853632184
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-04-06 14:51:18 +0200

    Fix test

--- test/core/test_visitors.py
+++ test/core/test_visitors.py
@@ -1,23 +1,21 @@
 import unittest
 from tuprolog.core import *
-from tuprolog.core.visitors import DefaultTermVisitor
+from tuprolog.core.visitors import AbstractTermVisitor
 import jpype.imports
 import kotlin.jvm.functions as _functions
 
-@jpype.JImplements(_functions.Function1)
-class Lambda:
-    def __init__(self, f):
-        self.f = f
-
-    @jpype.JOverride
-    def invoke(self, arg):
-        return self.f(arg)
+import unittest
+from tuprolog.core import *
+from tuprolog.core.visitors import AbstractTermVisitor
 
 
 class TestVisitors(unittest.TestCase):
 
     def test_term_visitor(self):
-        visitor = DefaultTermVisitor.of(Lambda(lambda term: 'a'))
+        class MyVisitor(AbstractTermVisitor):
+            def defaultValue(self, term):
+                return 'a'
+        visitor = MyVisitor()
         for term in [Atom.of('a'), Integer.of(1), Var.of("X")]:
             self.assertEqual(term.accept(visitor), 'a')
 
