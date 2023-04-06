c9a49029d9487443b028ff52771df1eb83e4a928
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-04-06 15:27:50 +0200

    Fix darwin jvm location

--- tuprolog/libs/__init__.py
+++ tuprolog/libs/__init__.py
@@ -10,7 +10,7 @@ CLASSPATH = Path(__file__).parent
 if platform.system() == 'Windows':
     JAVA_PATH = JAVA_HOME / 'bin' / 'server' / 'jvm.dll'
 elif platform.system() == 'Darwin':
-    JAVA_PATH = JAVA_HOME / 'lib' / 'libjli.dylib'
+    JAVA_PATH = JAVA_HOME / 'Contents' / 'Home' / 'lib' / 'jli' / 'libjli.dylib'
 elif platform.system() == 'Linux':
     JAVA_PATH = JAVA_HOME / 'lib' / 'server' / 'libjvm.so'
 else:
@@ -24,12 +24,3 @@ def install_java_if_missing() -> Path:
     installation_path = Path(jdk.install(java_version, jre=not java_version.startswith('16'), path=destination_folder))
     destination_folder = JAVA_HOME
     installation_path = installation_path.rename(destination_folder)
-    def list_files(startpath):
-        for root, dirs, files in os.walk(startpath):
-            level = root.replace(startpath, '').count(os.sep)
-            indent = ' ' * 4 * (level)
-            print('{}{}/'.format(indent, os.path.basename(root)))
-            subindent = ' ' * 4 * (level + 1)
-            for f in files:
-                print('{}{}'.format(subindent, f))
-    list_files(str(installation_path))
