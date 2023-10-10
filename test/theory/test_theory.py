7a0b8010cce24f6850c07c7bd21804ca22d48289
Luca Deluigi <lucadelu97@gmail.com>
2023-10-10 09:54:56 +0200

    Add unificator tests

---
+++ test/theory/test_theory.py
@@ -0,0 +1,43 @@
+import unittest
+from tuprolog.core import clause, var, struct, atom
+from tuprolog.jvmutils import Iterable
+from tuprolog.theory import theory
+
+class TestTheory(unittest.TestCase):
+    def test_empty_theory(self):
+        t = theory()
+        self.assertIsNotNone(t)
+        self.assertEqual(0, len(t))
+        self.assertTrue(t.is_empty)
+
+    def test_retract(self):
+        A = var('A')
+        a = atom('a')
+        f = lambda arg: struct('f', arg)
+        g = lambda arg: struct('g', arg)
+        t = theory([
+            c1 := clause(f(A), g(A)),
+            clause(g(a)),
+        ])
+        self.assertEqual(2, len(t))
+        result = t.retract(c1)
+        self.assertEqual(1, len(result.theory))
+
+    def test_unificator(self):
+        A = var('A')
+        a = atom('a')
+        f = lambda arg: struct('f', arg)
+        g = lambda arg: struct('g', arg)
+
+        t = theory([
+            clause(g(a)),
+        ])
+        results = list(t[f(a)])
+        self.assertEqual(0, len(results))
+
+        t = theory([
+            clause(f(A), g(A)),
+            clause(g(a)),
+        ])
+        results = list(t[f(a)])
+        self.assertEqual(1, len(results))
---
+++ tuprolog/utils/_definitions.py
@@ -0,0 +1,9 @@
+from tuprolog import logger
+import jpype.imports
+import it.unibo.tuprolog.utils as _utils # type: ignore
+
+Castable = _utils.Castable
+
+Taggable = _utils.Taggable
+
+logger.debug("Loaded JVM classes from it.unibo.tuprolog.utils.*")
